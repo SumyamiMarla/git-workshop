a503fcbf6f53ba86acdd2c9f6298fe4ad6ec2d44
SumyamiMarla <padma.marla@tigeranalytics.com>
2024-03-21 12:02:56 +0530

    Initial commit

--- simple_functions.py
+++ simple_functions.py
@@ -1,11 +1,19 @@
 # Custom python functions
 
 def double_number(a):
+    '''
+    This function doubles the number given as argument
+    params int a: number to be doubled.
+    '''
     print(f'value before double_number(): {a}')
     result=a+a
     print(f'value after double_number(): {result}')
 
 def square_number(a):
+    '''
+    This function squares the number given as argument
+    params int a: number to be squared.
+    '''
     print(f'value before square_number(): {a}')
     result=a*a
     print(f'value after square_number(): {result}')
