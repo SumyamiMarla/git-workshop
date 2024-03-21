f559ee808d298c050c44936db829265abceb2972
SumyamiMarla <padma.marla@tigeranalytics.com>
2024-03-21 16:31:23 +0530

    Build python packaging

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from simple_functions import double_number, square_number
+from src.mypackage.simple_functions import double_number, square_number
 a = 5
 result = double_number(a)
 result = square_number(a)
